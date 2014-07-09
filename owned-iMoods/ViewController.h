66e95f794e5f7fc2c2bac65c581aef28129b20fa
huyl <huyl@users.noreply.github.com>
2014-07-08 19:23:19 -0700

    Initial Commit

---
+++ owned-iMoods/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  owned-iMoods
+//
+//  Created by Huy on 7/8/14.
+//  Copyright (c) 2014 huy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ owned-iMoodsTests/owned_iMoodsTests.m
@@ -0,0 +1,34 @@
+//
+//  owned_iMoodsTests.m
+//  owned-iMoodsTests
+//
+//  Created by Huy on 7/8/14.
+//  Copyright (c) 2014 huy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface owned_iMoodsTests : XCTestCase
+
+@end
+
+@implementation owned_iMoodsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
